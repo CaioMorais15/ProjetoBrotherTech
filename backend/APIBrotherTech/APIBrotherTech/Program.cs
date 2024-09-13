d59499828efb99590cbafaa0eb574a91ac488756
CaioMorais <franciscocaio618@gmail.com>
2024-09-13 16:48:46 -0300

    api 2

--- backend/APIBrotherTech/APIBrotherTech/Program.cs
+++ backend/APIBrotherTech/APIBrotherTech/Program.cs
@@ -1,37 +1,44 @@
+using APIBrotherTech.Data;
 using ExemploAPI.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar e Configurar o CORS (Cross-Origin Resource Sharing)
-builder.Services.AddCors(options =>
+
+internal class Program
 {
-    options.AddPolicy("AllowAll", builder =>
+    private static void Main(string[] args)
     {
-        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-    });
-});
+        var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona Controladores Padrão
-builder.Services.AddControllers();
+        // Adicionar e Configurar o CORS (Cross-Origin Resource Sharing)
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+        });
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
+        // Adiciona Controladores Padrão
+        builder.Services.AddControllers();
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
     {
         new OpenApiSecurityScheme
         {
@@ -47,47 +54,49 @@ builder.Services.AddSwaggerGen(c =>
         },
         new List<string>()
         }
-    });
-});
+            });
+        });
 
 
-// Add Serviço de Conexao com o Banco de Dados MS SQL SERVER
-var strConn = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(strConn));
+        // Add Serviço de Conexao com o Banco de Dados MS SQL SERVER
+        var strConn = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(strConn));
 
-// Serviço de EndPoints do Identity Framework
-builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
-{
-    options.SignIn.RequireConfirmedEmail = false;
-    options.SignIn.RequireConfirmedAccount = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-})
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
-
-
-// Serviço de Autenticação
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
+        // Serviço de EndPoints do Identity Framework
+        builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedAccount = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+        })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
 
 
-var app = builder.Build();
+        // Serviço de Autenticação
+        builder.Services.AddAuthentication();
+        builder.Services.AddAuthorization();
 
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
 
-// Mapear Endpoints do IdentyUsers a partir de Grupos
-app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
-app.MapGroup("/roles").MapIdentityApi<IdentityRole>();
+        var app = builder.Build();
 
-app.UseHttpsRedirection();
+        // Configure the HTTP request pipeline.
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
-// Permitir autenticação e autorização a partir de qualquer origem
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseCors("AllowAll");
+        // Mapear Endpoints do IdentyUsers a partir de Grupos
+        app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
+        app.MapGroup("/roles").MapIdentityApi<IdentityRole>();
 
-app.MapControllers();
-app.Run();
+        app.UseHttpsRedirection();
+
+        // Permitir autenticação e autorização a partir de qualquer origem
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseCors("AllowAll");
+
+        app.MapControllers();
+        app.Run();
+    }
+}
