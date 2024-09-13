0561d1b73782f00d6605d9f317888957aa312c68
CaioMorais <franciscocaio618@gmail.com>
2024-09-13 16:48:14 -0300

    api finalizado

---
+++ backend/APIBrotherTech/APIBrotherTech/Models/EmailConfig.cs
@@ -0,0 +1,11 @@
+﻿using MailKit.Security;
+
+namespace ExemploAPI.Services
+{
+    public class EmailConfig
+    {
+        public string SmtpServer { get; internal set; }
+        public SaslMechanism UserName { get; internal set; }
+        public int Port { get; internal set; }
+    }
+}
