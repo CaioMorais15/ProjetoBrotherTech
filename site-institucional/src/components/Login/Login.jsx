28ecd425bcca9034dd3e542271d46678db5447bb
Fe-Corazza <felipecorazza09@gmail.com>
2024-09-26 14:20:54 -0300

    tela de login finalizada

--- site-institucional/src/components/Login/Login.jsx
+++ site-institucional/src/components/Login/Login.jsx
@@ -3,34 +3,41 @@ import { useState } from "react"
 import styles from "./Login.module.css"
 export const Login = () => {
 
-    const [pagina, setPagina] = useState('home');
+  const [pagina, setPagina] = useState('home');
 
-    const trocaPagina = (novaPagina) => {
-        setPagina(novaPagina)
-    }
-    return <>
-        <div className={styles.divLogin}>
-            <img src={logo} />
+  const trocaPagina = (novaPagina) => {
+    setPagina(novaPagina)
+  }
+  return <>
+    <div className={styles.container} >
+      <div className={styles.divLogin}>
 
-            <div id="Logo"></div>
-            <h1 className={styles.caixa}>Bem-Vindo à Brothers Tech</h1>
-            <div id="textoLogin">
+        <img src={logo} className={styles.logo} />
+        <div id="Logo">
 
-                <h1 className={styles.tituloEmail}>Login</h1>
-                <div id="Login">
-                    <input className={styles.campoEmail} type="E-Mail/Telefone" placeholder="E-Mail/Telefone..." />
-                </div>
-                <h1 className={styles.tituloSenha}>Senha</h1>
-                <div id="Senha">
-                    <input className={styles.campoEmail} type="password" placeholder="Senha..." />
-                </div>
-                <p>
-                    <input type="checkbox" /> Concordo com os Termos de Serviço e com a Política de Privacidade da Brothers Tech</p>
-                <button className={styles.campoEntrar} >Entrar</button>
-                <p>Ainda não tem uma conta?<a href="site/src/cadastro/Cadastrar.jsx" > Crie uma!</a></p>
+          <h1 className={styles.caixa}>Bem-Vindo à Brothers Tech</h1>
+          <div id="textoLogin">
+
+            <h1 className={styles.tituloEmail}>Login</h1>
+            <div id="Login">
+              <input className={styles.campoEmail} type="E-Mail/Telefone" placeholder="E-Mail/Telefone..." />
+            </div>
+            <h1 className={styles.tituloSenha}>Senha</h1>
+            <div id="Senha">
+              <input className={styles.campoEmail} type="password" placeholder="Senha..." />
+            </div>
+            <p>
+              <input type="checkbox" /> Concordo com os Termos de Serviço e com a Política de Privacidade da Brothers Tech</p>
+            <div id="Entrar">
+              <button className={styles.campoEntrar} >Entrar</button>
+              <p>Ainda não tem uma conta?<a href="site/src/cadastro/Cadastrar.jsx" > Crie uma!</a></p>
             </div>
           </div>
-    </>
+        </div>
+      </div>
+    </div>
+
+  </>
 }
 
 export default Login
