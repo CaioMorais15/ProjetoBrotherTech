b86ee2d4d4490013eaa6406a0c572282ab8a5adb
MuriloGaliazzi <murilogaliazzi1@gmail.com>
2024-09-27 14:33:37 -0300

    Criação da Tabela de Criptos na pagina Mercados

--- site-institucional/src/components/Mercados/Mercado.jsx
+++ site-institucional/src/components/Mercados/Mercado.jsx
@@ -1,13 +1,42 @@
-import "./Mercado.css"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './Mercado.css'
 
-function Mercados() {
+const Mercados = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://api.binance.com/api/v3/ticker/price');
+                setData(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar dados da Binance:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
- <div className="Container">
-   <div className="text">
-    <h1>  Visão Geral do Mercado</h1>
-    
-   </div>
- </div>
-    )
-}
-export default Mercados
+        <table>
+            <thead>
+                <tr>
+                    <th>Símbolo</th>
+                    <th>Preço</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item) => (
+                    <tr key={item.symbol}>
+                        <td>{item.symbol}</td>
+                        <td>{item.price}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default Mercados;
