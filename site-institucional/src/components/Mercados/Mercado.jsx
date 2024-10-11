bce1f3dad74465cf6781cfa776a999f0c2c751aa
CaioMorais <franciscocaio618@gmail.com>
2024-10-11 16:05:23 -0300

    projeto revisado

--- site-institucional/src/components/Mercados/Mercado.jsx
+++ site-institucional/src/components/Mercados/Mercado.jsx
@@ -4,21 +4,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Mercado.css'; 
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 const Mercados = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-
+  
   useEffect(() => {
     const   
     fetchData = async () => {
       setIsLoading(true);
       setError(null);
-
+      
       try {
         const response = await axios.get('https://api.binance.com/api/v3/ticker/price');   
-
+        
         setData(response.data.slice(0, 10)); // Limit to 10 items
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -31,8 +33,9 @@ const Mercados = () => {
     fetchData();
   }, []);
   
-  
   return (
+    <div>
+<Navbar />
     <div className="Page">
     <table className="mercado-table">
       <thead>
@@ -64,6 +67,8 @@ const Mercados = () => {
         )}
       </tbody>
     </table>
+        </div>
+    <Footer />
         </div>
   );
 };
